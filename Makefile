63f10f78f74a3bbe740e6f68c2c79c321b6f7229
rhl120 <rhl120@protonmail.com>
2022-12-05 17:49:05 +0100

    Solution to day5 part 1

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: day1 day2 day3 day4
+all: day1 day2 day3 day4 day5
 day1:
 	rustc ./solutions/day1.rs --out-dir ./bins/ && echo 
 day2:
@@ -7,5 +7,7 @@ day3:
 	rustc ./solutions/day3.rs --out-dir ./bins/
 day4:
 	rustc ./solutions/day4.rs --out-dir ./bins/
+day5:
+	rustc ./solutions/day5.rs --out-dir ./bins/
 clean:
 	rm -rf ./bins
---
+++ solutions/day5.rs
@@ -0,0 +1,104 @@
+type Crate = char;
+type Stack = Vec<Crate>;
+struct Instruction {
+    quant: usize,
+    dst: usize,
+    src: usize,
+}
+fn load_input() -> Option<String> {
+    std::fs::read_to_string("./inputs/day5").ok()
+}
+
+fn split_input(input: &str) -> Option<(String, String)> {
+    let mut input = input.split("\n\n");
+    Some((input.next()?.to_string(), input.next()?.to_string()))
+}
+
+//I just realized that all I could have just checked with the numbers on the last
+//line but of course I was too stupid to notice it before.
+fn parse_stack(input: &str) -> Option<Vec<Stack>> {
+    let mut ret = Vec::new();
+    let mut stacks = input.split("\n").collect::<Vec<&str>>();
+    stacks.pop();
+    for i in stacks {
+        let mut stack_idx = 0;
+        let mut n_chars = 0;
+        let mut parsing_crate = false;
+        for i in i.chars() {
+            if n_chars % 4 == 0 && n_chars != 0 {
+                stack_idx += 1;
+                n_chars = 0;
+            }
+            if parsing_crate {
+                if i.is_ascii_uppercase() {
+                    while ret.len() <= stack_idx {
+                        ret.push(Vec::new());
+                    }
+                    ret[stack_idx].push(i);
+                } else if i == ']' {
+                    parsing_crate = false;
+                } else {
+                    return None;
+                }
+            } else if i == '[' {
+                parsing_crate = true;
+            } else if i != ' ' {
+                return None;
+            }
+            n_chars += 1;
+        }
+    }
+    Some(ret)
+}
+
+fn parse_instructions(input: &str) -> Option<Vec<Instruction>> {
+    input
+        .trim()
+        .split("\n")
+        .map(|x| {
+            let v = x
+                .replace("move ", "")
+                .replace(" from ", " ")
+                .replace(" to ", " ")
+                .split(" ")
+                .map(|x| x.to_string())
+                .collect::<Vec<String>>();
+            Some(Instruction {
+                quant: v.get(0)?.parse::<usize>().ok()?,
+                src: v.get(1)?.parse::<usize>().ok()? - 1,
+                dst: v.get(2)?.parse::<usize>().ok()? - 1,
+            })
+        })
+        .collect::<Option<Vec<Instruction>>>()
+}
+
+fn part1_solve(stacks: &mut Vec<Stack>, instructs: &Vec<Instruction>) -> Option<String> {
+    for i in instructs {
+        for _ in 0..i.quant {
+            let a = stacks[i.src][0];
+            stacks[i.dst].insert(0, a);
+            stacks[i.src].remove(0);
+        }
+    }
+    stacks
+        .iter()
+        .map(|x| Some(x.get(0)?.to_owned()))
+        .collect::<Option<String>>()
+}
+
+fn main() {
+    if let Some(input) = load_input().and_then(|x| split_input(&x)) {
+        if let Some(instrucs) = parse_instructions(&input.1) {
+            if let Some(mut stacks) = parse_stack(&input.0) {
+                println!(
+                    "The solution to part 1 is: {}",
+                    part1_solve(&mut stacks, &instrucs).unwrap()
+                );
+            }
+        } else {
+            eprintln!("Failed to parse instructions");
+        }
+    } else {
+        eprintln!("Failed to load input");
+    }
+}
