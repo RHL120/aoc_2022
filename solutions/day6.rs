ec80f2b2e444cf09d37317886514af9f6ccb8418
rhl120 <rhl120@protonmail.com>
2022-12-06 11:58:32 +0100

    Unify the part1 and 2 solver

--- solutions/day6.rs
+++ solutions/day6.rs
@@ -2,11 +2,11 @@ fn load_input() -> Option<String> {
     std::fs::read_to_string("./inputs/day6").ok()
 }
 
-fn find_marker1(input: &str) -> Option<(String, usize)> {
+fn find_marker(input: &str, n_letters: usize) -> Option<(String, usize)> {
     let mut idx = 0;
     while idx < input.len() {
-        if idx >= 4 {
-            let ret = &input[idx - 4..idx];
+        if idx >= n_letters {
+            let ret = &input[idx - n_letters..idx];
             let mut is_starter = true;
             for chr in ret.chars() {
                 if ret.chars().filter(|x| x.clone() == chr).count() > 1 {
@@ -23,34 +23,14 @@ fn find_marker1(input: &str) -> Option<(String, usize)> {
     return None;
 }
 
-fn find_marker2(input: &str) -> Option<(String, usize)> {
-    let mut idx = 0;
-    while idx < input.len() {
-        if idx >= 14 {
-            let ret = &input[idx - 14..idx];
-            let mut is_starter = true;
-            for chr in ret.chars() {
-                if ret.chars().filter(|x| x.clone() == chr).count() > 1 {
-                    is_starter = false;
-                    break;
-                }
-            }
-            if is_starter {
-                return Some((ret.to_string(), idx));
-            }
-        }
-        idx += 1;
-    }
-    return None;
-}
 fn main() {
     let input = load_input().unwrap();
     println!(
         "The solution to part1 is: {:#?}",
-        find_marker1(&input).unwrap()
+        find_marker(&input, 4).unwrap()
     );
     println!(
         "The solution to part2 is: {:#?}",
-        find_marker2(&input).unwrap()
+        find_marker(&input, 14).unwrap()
     );
 }
