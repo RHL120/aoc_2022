d97f1d8ff5da81ac2a9cbf8266e0e53624f18e4e
rhl120 <rhl120@protonmail.com>
2022-12-06 06:52:53 +0100

    Solution to part 1 of day 6

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: day1 day2 day3 day4 day5
+all: day1 day2 day3 day4 day5 day6
 day1:
 	rustc ./solutions/day1.rs --out-dir ./bins/
 day2:
@@ -9,5 +9,7 @@ day4:
 	rustc ./solutions/day4.rs --out-dir ./bins/
 day5:
 	rustc ./solutions/day5.rs --out-dir ./bins/
+day6:
+	rustc ./solutions/day6.rs --out-dir ./bins/
 clean:
 	rm -rf ./bins
---
+++ solutions/day6.rs
@@ -0,0 +1,32 @@
+fn load_input() -> Option<String> {
+    std::fs::read_to_string("./inputs/day6").ok()
+}
+
+fn find_marker(input: &str) -> Option<(String, usize)> {
+    let mut idx = 0;
+    while idx < input.len() {
+        if idx >= 4 {
+            let ret = &input[idx - 4..idx];
+            let mut is_starter = true;
+            for chr in ret.chars() {
+                if ret.chars().filter(|x| x.clone() == chr).count() > 1 {
+                    is_starter = false;
+                    break;
+                }
+            }
+            if is_starter {
+                return Some((ret.to_string(), idx));
+            }
+        }
+        idx += 1;
+    }
+    return None;
+}
+
+fn main() {
+    let input = load_input().unwrap();
+    println!(
+        "The solution to part1 is: {:#?}",
+        find_marker(&input).unwrap()
+    );
+}
