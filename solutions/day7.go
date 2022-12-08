91df0695be7efd1d2217cc79d2ab3a9cdcb237be
rhl120 <rhl120@protonmail.com>
2022-12-08 21:24:40 +0100

    Stick to camelCase in golang files

--- solutions/day7.go
+++ solutions/day7.go
@@ -11,22 +11,21 @@ import (
 type fileStructure struct {
 	parent   *fileStructure
 	children map[string]*fileStructure
-	is_dir   bool
+	isDir    bool
 	size     int
 }
 
-func part1_solve(input string) (*fileStructure, error) {
+func constructTree(input string) (*fileStructure, error) {
 	lines := strings.Split(strings.TrimSpace(input), "\n")
 	ret := &fileStructure{
 		parent:   nil,
 		children: make(map[string]*fileStructure, 0),
-		is_dir:   true,
+		isDir:    true,
 		size:     0,
 	}
 	cwd := ret
 	listing := false
 	for idx, line := range lines {
-		_ = idx
 		if strings.HasPrefix(line, "$ cd ") {
 			listing = false
 			arg := strings.TrimPrefix(line, "$ cd ")
@@ -50,34 +49,28 @@ func part1_solve(input string) (*fileStructure, error) {
 		} else if listing {
 			var args = strings.Split(line, " ")
 			size := 0
-			is_dir := false
+			isDir := false
 			var children map[string]*fileStructure = nil
 			if len(args) != 2 {
 				return ret, fmt.Errorf("line: %d, %s: is not a valid ls entry", idx, line)
 			}
 			if args[0] == "dir" {
 				children = make(map[string]*fileStructure)
-				is_dir = true
+				isDir = true
 			} else {
 				var err error
 				size, err = strconv.Atoi(args[0])
 				if err != nil {
 					return ret, fmt.Errorf("line: %d, %s has an invalid size", idx, line)
 				}
-				parent := cwd
-				for {
-					if parent == nil {
-						break
-					}
+				for parent := cwd; parent != nil; parent = parent.parent {
 					parent.size += size
-					parent = parent.parent
-
 				}
 			}
 			cwd.children[args[1]] = &fileStructure{
 				parent:   cwd,
 				children: children,
-				is_dir:   is_dir,
+				isDir:    isDir,
 				size:     size,
 			}
 		}
@@ -85,31 +78,31 @@ func part1_solve(input string) (*fileStructure, error) {
 	return ret, nil
 }
 
-func traverse_pt1(f *fileStructure, size *int) {
-	if f.is_dir {
+func traversePt1(f *fileStructure, size *int) {
+	if f.isDir {
 		if f.size <= 100000 {
 			*size += f.size
 		}
 		for _, file := range f.children {
-			traverse_pt1(file, size)
+			traversePt1(file, size)
 		}
 	}
 }
 
-func traverse_pt2(f *fileStructure, wanted_size int, min *int) {
-	if f.is_dir {
-		if f.size >= wanted_size && f.size < *min {
+func traversePt2(f *fileStructure, wantedSize int, min *int) {
+	if f.isDir {
+		if f.size >= wantedSize && f.size < *min {
 			*min = f.size
 		}
 		for _, i := range f.children {
-			traverse_pt2(i, wanted_size, min)
+			traversePt2(i, wantedSize, min)
 		}
 	}
 }
 
-func solve_pt2(f *fileStructure) int {
+func solvePt2(f *fileStructure) int {
 	var size int = f.size
-	traverse_pt2(f, f.size-40000000, &size)
+	traversePt2(f, f.size-40000000, &size)
 	return size
 }
 
@@ -118,14 +111,14 @@ func main() {
 	if err != nil {
 		fmt.Println("Failed to read input")
 	}
-	solution, err := part1_solve(string(input[:]))
+	solution, err := constructTree(string(input[:]))
 	if err != nil {
 		fmt.Printf("error: %v\n", err)
 		return
 	}
 	var size = 0
-	traverse_pt1(solution, &size)
+	traversePt1(solution, &size)
 	fmt.Println("The solution to part1 is: ", size)
 	size = 0
-	fmt.Println("The solution to part2 is: ", solve_pt2(solution))
+	fmt.Println("The solution to part2 is: ", solvePt2(solution))
 }
