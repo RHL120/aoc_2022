9def7b0ef1409f595bc388403368667b52ef0203
rhl120 <rhl120@protonmail.com>
2022-12-08 11:24:23 +0100

    Intial solution for day8 part 2

--- solutions/day8.rs
+++ solutions/day8.rs
@@ -76,12 +76,82 @@ fn solve_part1(input: &str) -> Option<usize> {
     return Some(ret);
 }
 
+fn score(grid: &Vec<Vec<u32>>, x_len: usize, x: usize, y: usize) -> u32 {
+    let mut top = 0;
+    let mut bottom = 0;
+    let mut left = 0;
+    let mut right = 0;
+    for i in (0..y).rev() {
+        top += 1;
+        if grid[i][x] >= grid[y][x] {
+            break;
+        }
+    }
+    for i in y + 1..grid.len() {
+        bottom += 1;
+        if grid[i][x] >= grid[y][x] {
+            break;
+        }
+    }
+    for i in (0..x).rev() {
+        left += 1;
+        if grid[y][i] >= grid[y][x] {
+            break;
+        }
+    }
+    for i in x + 1..grid.len() {
+        right += 1;
+        if grid[y][i] >= grid[y][x] {
+            break;
+        }
+    }
+    return top * bottom * left * right;
+}
+
+fn part2_solve(input: &str) -> Option<u32> {
+    let grid = input
+        .trim()
+        .split("\n")
+        .map(|x| {
+            x.chars()
+                .map(|x| {
+                    if let Some(y) = x.to_digit(10) {
+                        Some(y)
+                    } else {
+                        println!("Bad char: {}", x);
+                        None
+                    }
+                })
+                .collect::<Option<Vec<u32>>>()
+        })
+        .collect::<Option<Vec<Vec<u32>>>>()?;
+    let mut max = 0;
+    let x_len = grid.get(0)?.len();
+    for (y, row) in grid.iter().enumerate() {
+        if row.len() != x_len {
+            return None;
+        }
+        for (x, _) in row.iter().enumerate() {
+            let s = score(&grid, x_len, x, y);
+            if s > max {
+                max = s
+            }
+        }
+    }
+    return Some(max);
+}
+
 fn main() {
     if let Some(input) = load_input() {
         if let Some(solution) = solve_part1(&input) {
+            println!("The solution to part 1 is: {}", solution)
+        } else {
+            eprintln!("Failed to solve part 1")
+        }
+        if let Some(solution) = part2_solve(&input) {
             println!("{}", solution)
         } else {
-            eprintln!("Failed to solve the problem")
+            eprintln!("Failed to solve part 2")
         }
     } else {
         eprintln!("Failed to load input")
