11c6d768edd3f8512dafae33aaa1e5a09a0836a1
rhl120 <rhl120@protonmail.com>
2022-12-09 19:22:22 +0100

    Clean up the day9 part 1 and prepare it for part 2

--- solutions/day9.rs
+++ solutions/day9.rs
@@ -1,5 +1,3 @@
-use std::collections::HashSet;
-
 #[derive(Debug)]
 enum Motion {
     Left(u32),
@@ -7,80 +5,8 @@ enum Motion {
     Up(u32),
     Down(u32),
 }
-
-struct Rope {
-    tail: (i32, i32),
-    head: (i32, i32),
-}
-
-impl Default for Rope {
-    fn default() -> Self {
-        Rope {
-            tail: (0, 0),
-            head: (0, 0),
-        }
-    }
-}
-
-impl Rope {
-    fn touches_head(&self, nt: (i32, i32)) -> bool {
-        (nt.0 - self.head.0).abs() <= 1 && (nt.1 - self.head.1).abs() <= 1
-    }
-    fn update_tail(&mut self) {
-        if self.touches_head(self.tail) {
-            return;
-        }
-        let ds = [
-            (0, -1),
-            (0, 1),
-            (-1, 0),
-            (1, 0),
-            (1, 1),
-            (-1, -1),
-            (1, -1),
-            (-1, 1),
-        ];
-        self.tail = ds
-            .iter()
-            .filter_map(|(dx, dy)| {
-                let nt = (self.tail.0 + dx, self.tail.1 + dy);
-                if self.touches_head(nt) {
-                    Some(nt)
-                } else {
-                    None
-                }
-            })
-            .min_by_key(|(x, y)| {
-                let a = x - self.head.0;
-                let b = y - self.head.1;
-                a * a + b * b
-            })
-            .unwrap()
-    }
-}
-
 fn load_input() -> Option<String> {
     std::fs::read_to_string("./inputs/day9").ok()
-}
-
-fn part1_solve(instrucs: &Vec<Motion>) -> usize {
-    let mut rope = Rope::default();
-    let mut hs = HashSet::new();
-    for motion in instrucs {
-        let (dx, dy, count) = match motion {
-            Motion::Left(x) => (-1, 0, x),
-            Motion::Right(x) => (1, 0, x),
-            Motion::Up(y) => (0, 1, y),
-            Motion::Down(y) => (0, -1, y),
-        };
-        for _ in 0..*count {
-            rope.head.0 += dx;
-            rope.head.1 += dy;
-            rope.update_tail();
-            hs.insert(rope.tail);
-        }
-    }
-    hs.len()
 }
 
 fn parse_input(input: &str) -> Option<Vec<Motion>> {
@@ -99,7 +25,71 @@ fn parse_input(input: &str) -> Option<Vec<Motion>> {
         })
         .collect()
 }
+fn are_neighbors((ax, ay): (i32, i32), (bx, by): (i32, i32)) -> bool {
+    (ax - bx).abs() <= 1 && (ay - by).abs() <= 1
+}
+
+fn update_rope(rope: &mut Vec<(i32, i32)>, idx: usize) {
+    if idx >= rope.len() || rope[idx] == rope[idx - 1] || are_neighbors(rope[idx], rope[idx - 1]) {
+        return;
+    }
+    rope[idx] = [
+        (0, -1),
+        (0, 1),
+        (-1, 0),
+        (1, 0),
+        (1, 1),
+        (-1, -1),
+        (1, -1),
+        (-1, 1),
+    ]
+    .iter()
+    .filter_map(|(dx, dy)| {
+        let new_knot = (rope[idx].0 + dx, rope[idx].1 + dy);
+        if are_neighbors(new_knot, rope[idx - 1]) {
+            Some(new_knot)
+        } else {
+            None
+        }
+    })
+    .min_by_key(|(x, y)| {
+        let a = x - rope[idx - 1].0;
+        let b = y - rope[idx - 1].1;
+        a * a + b * b
+    })
+    .unwrap();
+    update_rope(rope, idx + 1);
+}
+
+fn move_rope(rope: &mut Vec<(i32, i32)>, instructions: &Vec<Motion>) -> usize {
+    let mut visited = std::collections::HashSet::new();
+    for instruction in instructions {
+        let (dx, dy, count) = match instruction {
+            Motion::Left(x) => (-1, 0, x),
+            Motion::Right(x) => (1, 0, x),
+            Motion::Up(y) => (0, 1, y),
+            Motion::Down(y) => (0, -1, y),
+        };
+        for _ in 0..*count {
+            rope[0].0 += dx;
+            rope[0].1 += dy;
+            update_rope(rope, 1);
+            println!(
+                "head: ({}, {}), tail: ({}, {})",
+                rope[0].0, rope[0].1, rope[1].0, rope[1].1
+            );
+            let tail = rope[rope.len() - 1];
+            visited.insert(tail);
+        }
+    }
+    visited.len()
+}
+
 fn main() {
     let input = parse_input(&load_input().unwrap()).unwrap();
-    println!("{:#?}", part1_solve(&input));
+    let mut rope = Vec::new();
+    for _ in 0..2 {
+        rope.push((0, 0));
+    }
+    println!("{}", move_rope(&mut rope, &input));
 }
