1bfdeffd49a3cdb1e7a9479eea53b9655c0c65d0
rhl120 <rhl120@protonmail.com>
2022-12-14 22:23:37 +0100

    Solution to day14 part 2

--- solutions/day14.rs
+++ solutions/day14.rs
@@ -61,7 +61,6 @@ impl Grid {
                 }
             }
         }
-        println!("{}", abyss_y);
         Grid {
             rocks,
             sand,
@@ -77,7 +76,7 @@ impl Grid {
             .all(|x| self.contains(*x))
     }
     fn new_sand(&mut self) -> bool {
-        let mut p = (500, 1);
+        let mut p = (500, 0);
         while !self.is_at_rest(p) {
             let (x, y) = p;
             if y > self.abyss_y {
@@ -88,15 +87,77 @@ impl Grid {
                 .find(|x| !self.contains(**x))
                 .unwrap();
         }
-        println!("here: ({}, {})", p.0, p.1);
         assert!(self.sand.insert(p));
         return false;
+    }
+    fn is_at_rest_pt2(&self, (x, y): (isize, isize)) -> bool {
+        y + 1 >= self.abyss_y
+            || [(x, y + 1), (x - 1, y + 1), (x + 1, y + 1)]
+                .iter()
+                .all(|x| self.contains(*x))
+    }
+    fn new_sand_pt2(&mut self) -> bool {
+        let mut p = (500, 0);
+        while !self.is_at_rest_pt2(p) {
+            let (x, y) = p;
+            p = *[(x, y + 1), (x - 1, y + 1), (x + 1, y + 1)]
+                .iter()
+                .find(|x| !self.contains(**x))
+                .unwrap();
+        }
+        self.sand.insert(p)
+    }
+}
+
+impl Display for Grid {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut ret = Vec::new();
+        for (col, row) in &self.rocks {
+            let (col, row) = (*col as usize, *row as usize);
+            while self.rocks.len() < row {
+                ret.push(Vec::new());
+            }
+            while ret[0].len() < col {
+                for i in &mut ret {
+                    i.push('.');
+                }
+            }
+            ret[row][col] = '#'
+        }
+        for (col, row) in &self.sand {
+            let (col, row) = (*col as usize, *row as usize);
+            while ret.len() < row {
+                ret.push(Vec::new());
+            }
+            while ret[0].len() < col {
+                for i in &mut ret {
+                    i.push('.');
+                }
+            }
+            ret[row][col] = 'o'
+        }
+        for i in &ret {
+            for j in i {
+                write!(f, "{}", j)?;
+            }
+            writeln!(f)?;
+        }
+        Ok(())
     }
 }
 
 fn part1_solve(grid: &mut Grid) -> usize {
     let mut i = 0;
     while !grid.new_sand() {
+        i += 1;
+    }
+    i
+}
+
+fn part2_solve(grid: &mut Grid) -> usize {
+    let mut i = 0;
+    grid.abyss_y += 2;
+    while grid.new_sand_pt2() {
         i += 1;
     }
     i
@@ -114,4 +175,6 @@ fn main() {
     let input = load_input().and_then(|x| parse_input(&x)).unwrap();
     let mut grid = Grid::new(&input);
     println!("The solution to part 1 is: {}", part1_solve(&mut grid));
+    let mut grid = Grid::new(&input);
+    println!("The solution to part 2 is: {}", part2_solve(&mut grid));
 }
